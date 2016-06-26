5c33d677a1e5a0045b57a1a9c550f391df011b34
Vinicius Rogério Araujo Silva <31182306+vinicius-ras@users.noreply.github.com>
2016-06-26 16:18:40 +0000

    * Created the project "RAMvader-Darksiders", which will be a trainer made with the RAMvader library for the game Darksiders.

---
+++ RAMvader-Darksiders/TrainerApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "RAMvader-Darksiders" )]
+[assembly: AssemblyDescription( "RAMvader-Darksiders is a trainer for the game Darksiders." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Vinicius.RAS" )]
+[assembly: AssemblyProduct( "RAMvader-Darksiders" )]
+[assembly: AssemblyCopyright( "Copyright © Vinicius Rogério Araujo Silva 2016" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "d00ac875-92ee-4d2e-a08e-9746913333f4" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
